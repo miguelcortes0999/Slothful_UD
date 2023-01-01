91681c0ba55e144216f8e8587c19d6c5fca16f80
miguelcortes0999 <62513516+miguelcortes0999@users.noreply.github.com>
2023-01-01 01:30:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.2' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+VERSION = '0.0.4' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
 PACKAGE_NAME = 'Slothful_UD' #Debe coincidir con el nombre de la carpeta 
 AUTHOR = 'miguelcortes0999'
 AUTHOR_EMAIL = 'miguelcortes0999@gmail.com'
